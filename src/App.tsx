649230b1395dd06a19b96e14e13ff5b1c5da412e
Mukul <130670365+mukul7661@users.noreply.github.com>
2024-02-14 16:25:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>tesing 2</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Deployed testing
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Testing
-      </p>
-    </>
-  )
+  return <>Final Deployed url</>
 }
 
 export default App
